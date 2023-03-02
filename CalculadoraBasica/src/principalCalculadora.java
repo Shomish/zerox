ce1084c5f9bad370b6ff4c53471be9e3f38af630
Steven Huillca Cusi <Freleida@gmail.com>
2023-03-02 16:26:46 -0500

    Primeratarea arreglada

--- CalculadoraBasica/src/principalCalculadora.java
+++ CalculadoraBasica/src/principalCalculadora.java
@@ -2,85 +2,130 @@ import java.util.Scanner;
 
 public class principalCalculadora {
     public static void menu(){
+        System.out.println("\n ############## MENU PRINCIPAL ##############\n");
         System.out.println("0- Salir");
         System.out.println("1- Sumar");
         System.out.println("2- Restar");
         System.out.println("3- Multiplicar");
         System.out.println("4- Dividir");
-        System.out.print("   >> Ingrese una opción: ");
+        System.out.print("   " +
+                ">> Ingrese una opción");
     }
 
     public static int suma(int num1, int num2){
-        int suma1;
-        return suma1=num1 + num2;
+        return num1 + num2;
     }
     public static int resta(int num1, int num2){
-        int resta;
-        return resta=num1 - num2;
+        return num1 - num2;
     }
     public static int multiplicacion(int num1, int num2){
-        int multiplicacion1;
-        return multiplicacion1=num1 * num2;
+        return num1 * num2;
     }
     public static int division(int num1, int num2){
-        int division1;
-        return division1=num1 / num2;
+        return num1 / num2;
+    }
+
+    public static int leerEntero(String promt){
+        int entero = -999999999;
+        int cont = 0;
+        do{
+            try {
+                System.out.print(promt+": ");
+                entero = new Scanner(System.in).nextInt();
+                cont = 3;
+            } catch (Exception e) {
+                System.out.println("Numero inválido, intentelo nuevamente: ");
+                cont++; // cont = cont + 1
+            }
+        } while(cont != 3); // este while temrina cuando cont = 3
+        return entero;
     }
 
     public static void main(String[] args) {
+        int num1, num2 = 0;
         int opcion;
-        int num1;
-        int num2;
         do {
             menu();
-            opcion = new Scanner(System.in).nextInt();
+            opcion = leerEntero("");
             switch (opcion){
-
                 case 0:
-                    System.out.print(" Adios. Vuelve pronto ...");
+                    System.out.println(" Adios. Vuelve pronto ...");
                     break;
                 case 1:
                     System.out.println(" VAMOS A SUMAR ");
-                    System.out.println(" Ingrese el primer numero: ");
-                    num1 = new Scanner(System.in).nextInt();
-                    System.out.println(" Ingrese el segundo numero:");
-                    num2 = new Scanner(System.in).nextInt();
+                    num1 = leerEntero("  Ingrese el primer sumando");
+                    if(num1!=-999999999)
+                    {
+                        num2 = leerEntero("  Ingrese el segundo sumando");
+                        if (num2!=-999999999)
+                        {
+                            System.out.println("   El resultado es: "  +suma(num1, num2));
+                        }else
+                        {
+                            System.out.println("El valor no es entero, intentelo nuevamente: ");
+                        }
+                    }else
+                    {
+                        System.out.println("El valor no es entero, intentelo nuevamente: ");
+                    }
 
-                    System.out.println("la suma es: " + suma(num1,num2) );
                     break;
                 case 2:
                     System.out.println(" VAMOS A RESTAR ");
-
-                    System.out.println(" Ingrese el primer numero: ");
-                    num1 = new Scanner(System.in).nextInt();
-                    System.out.println(" Ingrese el segundo numero:");
-                    num2 = new Scanner(System.in).nextInt();
-
-                    System.out.println("la resta es: " + resta(num1,num2) );
+                    num1 = leerEntero("  Ingrese el primer Número");
+                    if(num1!=-999999999)
+                    {
+                        num2 = leerEntero("  Ingrese el segundo numero");
+                        if (num2!=-999999999)
+                        {
+                            System.out.println("   El resultado es: "  +resta(num1, num2));
+                        }else
+                        {
+                            System.out.println("El valor no es entero, intentelo nuevamente: ");
+                        }
+                    }else
+                    {
+                        System.out.println("El valor no es entero, intentelo nuevamente: ");
+                    }
                     break;
                 case 3:
-                    System.out.println(" VAMOS A MULTIPLICAR ");
-
-                    System.out.println(" Ingrese el primer numero: ");
-                    num1 = new Scanner(System.in).nextInt();
-                    System.out.println(" Ingrese el segundo numero:");
-                    num2 = new Scanner(System.in).nextInt();
-
-                    System.out.println("la multiplicacion  es: " + multiplicacion(num1,num2) );
+                    System.out.println(" VAMOS A multiplicar ");
+                    num1 = leerEntero("  Ingrese el primer Número");
+                    if(num1!=-999999999)
+                    {
+                        num2 = leerEntero("  Ingrese el segundo numero");
+                        if (num2!=-999999999)
+                        {
+                            System.out.println("   El resultado es: "  +resta(num1, num2));
+                        }else
+                        {
+                            System.out.println("El valor no es entero, intentelo nuevamente: ");
+                        }
+                    }else
+                    {
+                        System.out.println("El valor no es entero, intentelo nuevamente: ");
+                    }
                     break;
                 case 4:
                     System.out.println(" VAMOS A DIVIDIR ");
-
-                    System.out.println(" Ingrese el primer numero: ");
-                    num1 = new Scanner(System.in).nextInt();
-                    System.out.println(" Ingrese el segundo numero:");
-                    num2 = new Scanner(System.in).nextInt();
-
-                    System.out.println("la division es: " + division(num1,num2) );
+                    num1 = leerEntero("  Ingrese el primer Número");
+                    if(num1!=-999999999)
+                    {
+                        num2 = leerEntero("  Ingrese el segundo numero");
+                        if (num2!=-999999999)
+                        {
+                            System.out.println("   El resultado es: "  +resta(num1, num2));
+                        }else
+                        {
+                            System.out.println("El valor no es entero, intentelo nuevamente: ");
+                        }
+                    }else
+                    {
+                        System.out.println("El valor no es entero, intentelo nuevamente: ");
+                    }
                     break;
                 default:
                     System.out.print("Opcion inválida. Intentelo nuevamente: ");
-
                     break;
             }
         } while( opcion != 0 );
